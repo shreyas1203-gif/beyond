bf0f20a8c33065fb30e6dc676f5fb77839d35352
shreyas <shreyasmohite1203@gmail.com>
2022-03-23 23:35:50 +0530

    class is created

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
+class Hello{
+    string str;
+    public:
+        Hello(string s){
+            str=s;
+            cout<<"Hello "<<s;
+        }
+
+};
 int main()
 {
-    cout<<"Hello";
+    Hello greeting("Shreyas");
+    
 }
